e0d28f30075d0cef5b1d2d781e6be79b463bfab4
Erislandio <erislandiosoares21@gmail.com>
2022-01-03 11:02:51 -0300

    feat: add express and graphqlSever

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,30 @@
-import { MikroORM } from "@mikro-orm/core"
-import mikroConfig from "./mikro-orm.config"
+import { MikroORM } from "@mikro-orm/core";
+import mikroConfig from "./mikro-orm.config";
+import express from 'express';
+import { ApolloServer } from 'apollo-server-express';
+import { buildSchema } from 'type-graphql';
+import { HelloResolver } from "./resolvers/hello";
 
 async function main() {
     const orm = await MikroORM.init(mikroConfig);
-    console.log(orm);
+    await orm.getMigrator().up();
+
+    const app = express();
+
+
+    const apolloServer = new ApolloServer({
+        schema: await buildSchema({
+            resolvers: [HelloResolver],
+            validate: false
+        })
+    });
+
+    apolloServer.applyMiddleware({ app });
+
+    app.listen(8000, () => {
+        console.log('app listen on localhost:8000');
+        console.log('graphql listen on localhost:8000/graphql');
+    });
 };
 
 main().catch(error => console.error(error));
